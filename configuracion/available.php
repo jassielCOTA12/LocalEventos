76f5959e17c1db5fe5ab572f7e589c67a6d5a994
OscarIst21 <oscaruriel_22@alu.uabcs.mx>
2024-12-04 17:49:15 -0800

    imamgenes de local en buscador

--- configuracion/available.php
+++ configuracion/available.php
@@ -7,7 +7,18 @@ if (!isset($_POST['fecha']) || empty($_POST['fecha'])) {
     echo "<p>Por favor, selecciona una fecha.</p>";
     exit();
 }
-
+$imagenesLocales = [
+    1 => 'imagenesLocales/SalonFiestaBonita.png',
+    2 => 'imagenesLocales/JardinLosPinos.jpg',
+    3 => 'imagenesLocales/SalonsLasEstrellas.jpg',
+    4 => 'imagenesLocales/FiestaKids.jpg',
+    5 => 'imagenesLocales/BodasDeluxe.jpg',
+    6 => 'imagenesLocales/SalonElegance.jpeg',
+    7 => 'imagenesLocales/SalonAventuras.jpg',
+    8 => 'imagenesLocales/CentroCorporativoElite.jpg',
+    9 => 'imagenesLocales/SalonFiestaFeliz.jpeg',
+    10 => 'imagenesLocales/HaciendaLosEncinos.jpg',
+];
 $fecha = $_POST['fecha'];
 
 try {
@@ -30,10 +41,13 @@ try {
         echo "<p>No hay locales disponibles para esta fecha.</p>";
     } else {
         foreach ($locales as $local) {
+            $idLocal = $local['id_local'];
+            $imagen = isset($imagenesLocales[$idLocal]) ? $imagenesLocales[$idLocal] : 'imagenes/imgLocal1.png'; 
+        
             echo "
-                <a href='local.php?id={$local['id_local']}'>
+                <a href='local.php?id={$idLocal}'>
                     <article class='localCard-gen'>
-                        <img src='imagenes/imgLocal1.png' alt='imagen de local no cargada'>
+                        <img src='{$imagen}' alt='imagen de local no cargada'>
                         <h3>{$local['nombre']}</h3>
                         <p>Hasta {$local['capacidad_maxima']} personas</p>
                         <p>Desde \${$local['precio_base']}</p>
